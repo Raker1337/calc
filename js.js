011266a2bade4914f0a33148b5ae74635fed013b
Raker1337 <104013956+Raker1337@users.noreply.github.com>
2022-10-02 22:30:04 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,53 @@
+
+    
+    var field = document.querySelectorAll('.field')
+    for (let i = 0; i < field.length; i++) {
+        var rangeInput = document.querySelectorAll('.range-input input')
+    var progress = document.querySelectorAll('.slider .progress')
+    var price = document.querySelectorAll(' .inputprice')
+   var mark = document.querySelector('#mark')
+   var and = 0
+   var and1 = 0
+   var and2 = 0
+   var sum = document.querySelector('.sum h2')
+   var pay = document.querySelector('.pay h2')
+    rangeInput.forEach(input=>{
+        input.addEventListener("input",()=>{
+            let minVal = parseInt(rangeInput[i].value)
+  progress[i].style.width=((minVal-rangeInput[i].min)/(rangeInput[i].max-rangeInput[i].min)*100)+ '%'
+            minVal
+            price[i].value = minVal
+            var pays = ((and-and1)*((0.035*Math.pow((1+0.035),and2))/(Math.pow((1+0.035),and2)-1))).toFixed(0)
+            pay.innerHTML = (((and-and1)*((0.035*Math.pow((1+0.035),and2))/(Math.pow((1+0.035),and2)-1))).toFixed(0))+'₽';
+            sum.innerHTML = ((and1+and2*pays).toFixed(0))+'₽'
+            
+            if (i==0){
+            and = minVal
+        }
+        if (i==1){
+            and1 = minVal
+        }
+        if (i==2){
+            and2 = minVal
+        }
+            if (i==1){
+     mark.innerHTML = minVal + '%'
+    price[1].value=(minVal*(and/100)).toFixed(0)
+    }
+
+        })
+    })
+    }
+    var button = document.querySelector('.button')
+    button.addEventListener('click', async _ =>{
+        try{
+            var responce = await fetch('https://httpbin.org/post',{
+                method:'post',
+                body:pay.innerHTML
+                
+            })
+            alert('completed')
+        }catch(err){
+            alert(err)
+        }
+    })
